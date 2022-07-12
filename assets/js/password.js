bf3b30f67993f44f7bd346a124e782fca8eb866a
jamielsp <jamiel1819@outlook.com>
2022-07-12 00:21:33 -0400

    Subiendo Amazing Books v1.1

--- assets/js/password.js
+++ assets/js/password.js
@@ -1,12 +1,12 @@
-function passwordCheck(){
-    var password = prompt("Please enter the password.");
-    if (password==="ilikepie"){
-        window.location="/writeups/hackthebox/cap/cap_writeup.html";
-    } else if (password!='' && password!=null) {
-        while(password !=="ilikepie"){
-            password = prompt("Please enter the password.");
-        }
-        window.location="jamiel.html";
-    }
-}
-window.onload=passwordCheck;
+// function passwordCheck(){
+//    var password = prompt("Please enter the password.");
+//    if (password==="ilikepie"){
+//        window.location="/writeups/hackthebox/cap/cap_writeup.html";
+//    } else if (password!='' && password!=null) {
+//        while(password !=="ilikepie"){
+//            password = prompt("Please enter the password.");
+//        }
+//        window.location="jamiel.html";
+//    }
+//}
+// window.onload=passwordCheck;
